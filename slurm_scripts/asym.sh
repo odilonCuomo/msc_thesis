c250d54df9c7ff30bbe6a6137ec225f100196ad2
Raphael Selz <kebl7471@htc-login02.arcus-htc.arc.local>
2023-08-17 16:20:07 +0100

    Add slurm scripts for noise and asymmetry

---
+++ slurm_scripts/asym.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set the number of nodes
+#SBATCH --nodes=1
+# set max wallclock time
+#SBATCH --time=23:00:00
+# set name of job
+#SBATCH --job-name=asymmetry
+# partition priority
+#SBATCH --partition=short
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+# send mail to this address
+#SBATCH --mail-user=raph.selz@gmail.com
+
+#activate the environment
+source proj_venv/bin/activate
+# run the application
+python gs_experiments/asymmetry.py
